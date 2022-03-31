b408e06a33a0d08a637d2d95758a1ccdec1c3dac
Torben <torben@brueck-web.de>
2022-03-31 14:14:56 +0200

    removed debug console outputs

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,9 @@ import {
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
+import { NotebookActions } from '@jupyterlab/notebook';
 
+import { ICodeCellModel } from '@jupyterlab/cells';
 
 /**
  * Initialization data for the jupyter_jmp_to_err extension.
@@ -12,7 +14,27 @@ const plugin: JupyterFrontEndPlugin<void> = {
   id: 'jupyter_jmp_to_err:plugin',
   autoStart: true,
   activate: (app: JupyterFrontEnd) => {
-    console.log('JupyterLab extension jupyter_jmp_to_err is activated!');
+
+    NotebookActions.selectionExecuted.connect(async (_: any, args: { lastCell: any; notebook: any; }) => {
+      const { lastCell } = args;
+
+      console.log(lastCell);
+
+      const codeCellModel = lastCell.model as ICodeCellModel;
+
+      for(let i = 0; i < codeCellModel.outputs.length; i++) {
+        const outputObject = codeCellModel.outputs.get(i).toJSON();
+
+        if(outputObject && outputObject.output_type == 'error') {
+
+          var element = lastCell.promptNode;
+
+          if(element !== null) {
+            element.scrollIntoViewIfNeeded();
+          }
+        }
+      }
+    });
   }
 };
 
